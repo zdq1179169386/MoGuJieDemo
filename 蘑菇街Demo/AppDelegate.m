a2a49cd9e88d39a25bd36619b49ff38166eb4ff7
ZhuDeQiang <ZhuDeQiang@zdqdeMacBook-Pro-MD104.local>
2018-04-02 22:04:55 +0800

    add /zdq/dev

--- 蘑菇街Demo/AppDelegate.m
+++ 蘑菇街Demo/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    //zdq/dev分支提交代码
     return YES;
 }
 
